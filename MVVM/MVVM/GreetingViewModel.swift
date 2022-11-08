c5474971f90a042e6a90b0d9d8f8e0c896a44af3
양준수 <yangjs@markany.com>
2022-11-08 18:33:25 +0900

    MVVM

--- MVVM/MVVM/GreetingViewModel.swift
+++ MVVM/MVVM/GreetingViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+protocol GreetingViewModelProtocol: class{
+    var greeting: String? {get}
+    var greetingDidChange:((GreetingViewModelProtocol)->())? {get set}
+    init(person:Person)
+    func showGreeting()
+}
+class GreetingViewModel: GreetingViewModelProtocol{
+    let person:Person
+    var greeting: String?{
+        didSet{
+            self.greetingDidChange?(self)
+        }
+    }
+    var greetingDidChange: ((GreetingViewModelProtocol) -> ())?
+    required init(person: Person) {
+        self.person = person
+    }
+    @objc func showGreeting() {
+        self.greeting = "Hello \(self.person.firstName) \(self.person.lastName)"
+    }
+    
+}
--- MVVM/MVVM/ViewController.swift
+++ MVVM/MVVM/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
-
+struct Person{
+    let firstName:String
+    let lastName:String
+}
 class ViewController: UIViewController {
-
+    var viewModel: GreetingViewModelProtocol!{
+        didSet{
+            self.viewModel.greetingDidChange = {[unowned self] vm in
+                self.greetingLabel.text = vm.greeting
+            }
+        }
+    }
+    @IBOutlet weak var showGreetingButton: UIButton!
+    @IBOutlet weak var greetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let model = Person(firstName: "YANG", lastName: "JOONSOO")
+        viewModel = GreetingViewModel(person: model)
+        self.showGreetingButton.addTarget(self, action: #selector(showGreeting), for: .touchUpInside)
     }
-
-
+    @objc func showGreeting(){
+        viewModel.showGreeting()
+    }
 }
 
